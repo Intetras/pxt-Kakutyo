9095469df786264d339d2b4d67a5109420ce9631
Intetras <77957619+Intetras@users.noreply.github.com>
2024-02-04 11:14:26 +0900

    更新 main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
 //% weight=30 icon="\uf0eb" color=#000080 block="KakutyoBlockTop"
 namespace kakutyoNameSpace {
+
+    //% group="kakutyoGroupA"
+    //% blockId="kakutyo1BlockId"
+    //% block="kakutyo1Block"
+    export function kakutyo() {
+        //何にもしない関数
+    }
 }
