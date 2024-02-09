b6f80c39eaceb6f5554adede3e98a5b0342b7fb3
Intetras <77957619+Intetras@users.noreply.github.com>
2024-02-09 15:01:46 +0900

    更新 main.ts

--- main.ts
+++ main.ts
@@ -2,18 +2,26 @@
 namespace kakutyoNameSpace {
 
     //% group="kakutyoGroupA"
-    //% blockId="kakutyo1BlockId"
-    //% block="kakutyo1Block"
-    export function kakutyo() {
+    //% blockId="kakutyoA1BlockId"
+    //% block="kakutyoA1Block"
+    export function kakutyoA1() {
         //値を返さない関数
         
     }
 
-    //% group="kakutyoGroupB"
-    //% blockId="kakutyo2BlockId"
-    //% block="kakutyo2Block"
-    export function kakutyo2(retNum:number) {
-        //値を返す関数
+    //% group="kakutyoGroupA"
+    //% blockId="kakutyoA2BlockId"
+    //% block="kakutyoA2Block %Num"
+    export function kakutyoA2(Num:number) {
+        //値を返さない関数
+        //引数ひとつ
+    }
 
+    //% group="kakutyoGroupB"
+    //% blockId="kakutyoB1BlockId"
+    //% block="kakutyoB1Block"
+    export function kakutyoB1(retNum:number) {
+        //値を返す関数
+        return 1
     }
 }
