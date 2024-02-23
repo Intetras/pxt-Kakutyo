c715de19eba9e25f19a633adf3bab24f775ca337
Intetras <77957619+Intetras@users.noreply.github.com>
2024-02-23 18:07:55 +0900

    更新 main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,19 @@
 //% weight=30 icon="\uf0eb" color=#000080 block="KakutyoBlockTop"
 namespace kakutyoNameSpace {
 
-    export enum list1{
+    enum list1{
         //% block="リスト1のひとつめ"
         First = 0,
         //% block="リスト1のふたつめ"
         Second = 1
     }
 
-    export enum list2 {
+    enum list2 {
         First = 0,
         Second = 1
     }
 
     //% group="kakutyoGroupA"
-    //% weight=1
     //% blockId="kakutyoA1BlockId"
     //% block="kakutyoA1Block"
     export function kakutyoA1() {
@@ -23,7 +22,6 @@ namespace kakutyoNameSpace {
     }
 
     //% group="kakutyoGroupA"
-    //% weight=2
     //% blockId="kakutyoA2BlockId"
     //% block="kakutyoA2Block %Num"
     export function kakutyoA2(Num:number) {
@@ -32,21 +30,13 @@ namespace kakutyoNameSpace {
     }
 
     //% group="kakutyoGroupA"
-    //% weight=3
     //% blockId="kakutyoA3BlockId"
     //% block="kakutyoA3Block"
     export function kakutyoA3(handler:()=>void) {
         //値を返さない関数
         //●●の場合
         input.onButtonPressed(Button.A,()=>{handler();})
-        //control.onEvent(
-        //    1,
-        //    EventBusValue.MICROBIT_EVT_ANY,
-        //    () => {
-        //        handler();
-        //    }
-
-        //)
+        
     }
 
     //% group="kakutyoGroupB"
